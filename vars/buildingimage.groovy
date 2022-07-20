b6404b1a0f24d2495b592d495eb8230a07b54aae
root <root@localhost.localdomain>
2022-07-20 17:57:30 +0200

    first commit

---
+++ vars/buildingimage.groovy
@@ -0,0 +1,7 @@
+#!/usr/bin/env groovy
+def call(String imageName) { 
+	sh ' docker build -t $imageName . '
+	
+
+}
+
---
+++ vars/pushingimage.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+def call(String imageName) {
+
+	echo "building docker image "
+	 withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                    sh " echo   $PASSWORD  | docker login -u $USERNAME --password-stdin "
+                    sh ' docker push $imageName 
+                   }
+}
+
+
+	 	
