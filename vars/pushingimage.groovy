2a70ec77c59645f831cf320e755996a573152325
Bedier1 <92409819+Bedier1@users.noreply.github.com>
2022-07-21 12:53:53 +0200

    Update pushingimage.groovy

--- vars/pushingimage.groovy
+++ vars/pushingimage.groovy
@@ -2,7 +2,7 @@
 def call(String imageName) {
 
 	echo "building docker image "
-	 withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+	 withCredentials([usernamePassword(credentialsId: 'docker-repo', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
                     sh " echo   $PASSWORD  | docker login -u $USERNAME --password-stdin "
                     sh " docker push $imageName "
                    }
